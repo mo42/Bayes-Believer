23ab68e55da88ae706b0a329dc4da27746bf4f21
Moritz Knabben <me@mo42.de>
2021-07-12 12:04:32 +0200

    Add first project files

---
+++ Bayes Believer/ViewModels/ListViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  ListViewModel.swift
+//  Bayes Believer
+//
+//  Created by Moritz on 09.07.21.
+//
+
+import Foundation
+
+class ListViewModel: ObservableObject {
+    
+    @Published var beliefs: [Belief] = []
+    
+    init() {
+      // Test belief for developing
+      beliefs.append(contentsOf: [Belief(description: "Belief", prior: 0.5, observationTrueLikelihood: 0.9, observationFalseLikelihood: 0.7)])
+    }
+    
+    func deleteBelief(indexSet: IndexSet) {
+        beliefs.remove(atOffsets: indexSet)
+    }
+    
+    func moveBelief(from: IndexSet, to: Int) {
+        beliefs.move(fromOffsets: from, toOffset: to)
+    }
+    
+    func addBelief(description: String, prior: Float) {
+      beliefs.append(Belief(description: description, prior: prior, observationTrueLikelihood: 0.0, observationFalseLikelihood: 0.0))
+    }
+}
---
+++ Bayes Believer/Views/BeliefListView.swift
@@ -0,0 +1,37 @@
+//
+//  ListView.swift
+//  Bayes Believer
+//
+//  Created by Moritz on 09.07.21.
+//
+
+import SwiftUI
+
+struct BeliefListView: View {
+  
+  @EnvironmentObject var listViewModel: ListViewModel
+  
+  var body: some View {
+    List {
+      ForEach(listViewModel.beliefs) { belief in
+        BeliefView(belief: belief)
+      }
+      .onDelete(perform: listViewModel.deleteBelief)
+      .onMove(perform: listViewModel.moveBelief)
+    }
+    .listStyle(PlainListStyle())
+    .navigationTitle("Bayes Believer")
+    .navigationBarItems(
+      leading: EditButton(),
+      trailing: NavigationLink("Add", destination: AddBeliefView()))
+  }
+}
+
+struct ListView_Previews: PreviewProvider {
+  static var previews: some View {
+    NavigationView {
+      BeliefListView()
+    }
+    .environmentObject(ListViewModel())
+  }
+}
