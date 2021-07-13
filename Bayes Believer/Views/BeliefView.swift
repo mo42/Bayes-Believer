12b53931ea7857a037a62d6484b9f22a1f5aaf67
Moritz Winter <me@mo42.de>
2021-07-13 19:31:24 +0200

    Add elements for observations

--- Bayes Believer/Views/BeliefView.swift
+++ Bayes Believer/Views/BeliefView.swift
@@ -8,22 +8,34 @@
 import SwiftUI
 
 struct BeliefView: View {
-  
   let belief: Belief
   
   var body: some View {
       NavigationLink(destination: BeliefUpdateView(belief: belief)) {
-        HStack {
-          Text("\(belief.prior, specifier: "%.2f")")
-          Text(belief.description).lineLimit(1)
-        }
+        BeliefItem(belief: belief)
         Spacer()
       }
     }
 }
 
+struct BeliefItem: View {
+  let belief: Belief
+  
+  var body: some View {
+    HStack {
+      Image(systemName: "circle.fill")
+        .foregroundColor(.blue)
+      Image(systemName: "circle.fill")
+        .foregroundColor(.red)
+      Text("\(belief.prior, specifier: "%.2f")")
+      Text(belief.description).lineLimit(1)
+      Spacer()
+    }
+  }
+}
+
 struct ListRowView_Previews: PreviewProvider {
-  static var belief = Belief(description: "Does god exist?", prior: 0.5, observationTrueLikelihood: 0.0, observationFalseLikelihood: 0.0)
+  static var belief = Belief(description: "Belief", prior: 0.5, observationTrueLikelihood: 0.0, observationFalseLikelihood: 0.0)
   static var previews: some View {
     BeliefView(belief: belief)
   }
