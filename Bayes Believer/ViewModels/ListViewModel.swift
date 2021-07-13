80f1abd0fe2084aab6fc69fb8aec458bf313e154
Moritz Knabben <me@mo42.de>
2021-07-13 19:31:24 +0200

    Add elements for observations

--- Bayes Believer/ViewModels/ListViewModel.swift
+++ Bayes Believer/ViewModels/ListViewModel.swift
@@ -8,23 +8,27 @@
 import Foundation
 
 class ListViewModel: ObservableObject {
+  
+  @Published var beliefs: [Belief] = []
+  
+  init() {
+    // Test belief for developing
+    beliefs.append(contentsOf: [Belief(description: "Belief", prior: 0.5, observationTrueLikelihood: 0.9, observationFalseLikelihood: 0.7)])
+  }
+  
+  func deleteBelief(indexSet: IndexSet) {
+    beliefs.remove(atOffsets: indexSet)
+  }
+  
+  func moveBelief(from: IndexSet, to: Int) {
+    beliefs.move(fromOffsets: from, toOffset: to)
+  }
+  
+  func addBelief(description: String, prior: Float) {
+    beliefs.append(Belief(description: description, prior: prior, observationTrueLikelihood: 0.0, observationFalseLikelihood: 0.0))
+  }
+  
+  func updateBelief(observation: Bool) {
     
-    @Published var beliefs: [Belief] = []
-    
-    init() {
-      // Test belief for developing
-      beliefs.append(contentsOf: [Belief(description: "Belief", prior: 0.5, observationTrueLikelihood: 0.9, observationFalseLikelihood: 0.7)])
-    }
-    
-    func deleteBelief(indexSet: IndexSet) {
-        beliefs.remove(atOffsets: indexSet)
-    }
-    
-    func moveBelief(from: IndexSet, to: Int) {
-        beliefs.move(fromOffsets: from, toOffset: to)
-    }
-    
-    func addBelief(description: String, prior: Float) {
-      beliefs.append(Belief(description: description, prior: prior, observationTrueLikelihood: 0.0, observationFalseLikelihood: 0.0))
-    }
+  }
 }
