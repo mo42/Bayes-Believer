15a92b83f45b1b2f26d95d5c486cf9e208fd961f
Moritz Winter <me@mo42.de>
2021-07-18 17:36:11 +0200

    Extract subview to remove duplicate code

--- Bayes Believer/Views/UpdateBeliefView.swift
+++ Bayes Believer/Views/UpdateBeliefView.swift
@@ -10,82 +10,77 @@ import SwiftUI
 struct BeliefUpdateView: View {
   
   let belief: Belief
-  @EnvironmentObject var listViewModel: ListViewModel
+  
   @State var isEditing = false
-  @State private var truePositive: Float
-  @State private var falsePositive: Float
-  
-  init(belief: Belief) {
-    self.belief = belief
-    truePositive = belief.truePositive
-    falsePositive = belief.falsePositive
-  }
-  
-  let formatter = createFormatter()
   
   var body: some View {
     ScrollView {
       VStack {
-        Text(belief.description)
-          .multilineTextAlignment(.center)
+        LikelihoodUpdate(belief: belief, observation: true)
         Divider()
+        LikelihoodUpdate(belief: belief, observation: false)
+      }
+      .padding(14.0)
+      .navigationTitle("Update Belief")
+    }
+  }
+  
+  struct LikelihoodUpdate: View {
+    let belief: Belief
+    let observation: Bool
+    let formatter: Formatter
+    
+    @State private var likelihood: Float
+    @EnvironmentObject var listViewModel: ListViewModel
+    
+    init(belief: Belief, observation: Bool) {
+      self.belief = belief
+      self.observation = observation
+      self.formatter = createFormatter()
+      if (observation) {
+        likelihood = belief.truePositive
+      } else {
+        likelihood = belief.falsePositive
+      }
+    }
+    
+    func updateLikelihood(likelihood: Float) {
+      if(observation) {
+        listViewModel.updateBelief(belief: belief.updateTruePositive(truePositive: likelihood))
+      } else {
+        listViewModel.updateBelief(belief: belief.updateFalsePositive(falsePositive: likelihood))
+      }
+    }
+    
+    var body: some View {
+      let binding = Binding(
+        get: { likelihood },
+        set: { newLikelihood in
+          likelihood = newLikelihood
+          updateLikelihood(likelihood: newLikelihood)
+        })
+      Text(belief.description)
+        .multilineTextAlignment(.center)
+      Divider()
+      if (observation) {
         Text("Suppose this is true, how likely is it to observe the evidence?")
           .multilineTextAlignment(.leading)
           .padding(0.0)
-        HStack {
-          TextField("", value: Binding(get: {
-            truePositive
-          }, set: { newTruePositive in
-            truePositive = newTruePositive
-            listViewModel.updateBelief(belief: belief.updateTruePositive(truePositive: newTruePositive))
-          }), formatter: formatter)
-            .keyboardType(UIKeyboardType.decimalPad)
-            .padding(.horizontal)
-            .frame(width: 70, height: 55)
-            .background(Color(UIColor.systemGray5))
-            .cornerRadius(10)
-          Slider(value: Binding(get: {
-            truePositive
-          }, set: { newTruePositive in
-            truePositive = newTruePositive
-            listViewModel.updateBelief(belief: belief.updateTruePositive(truePositive: newTruePositive))
-          }),
-                 in: 0.0...1.0,
-                 onEditingChanged: { editing in
-                    isEditing = editing
-                 })
-        }
-        Divider()
+      } else {
         Text("Suppose this is false, how likely is it to observe the evidence?")
           .multilineTextAlignment(.leading)
           .padding(0.0)
-        HStack {
-          TextField("", value: Binding(get: {
-            falsePositive
-          }, set: { newObservationFalseLikelihood in
-            falsePositive = newObservationFalseLikelihood
-            listViewModel.updateBelief(belief: belief.updateFalsePositive(falsePositive: newObservationFalseLikelihood))
-          }), formatter: formatter)
-            .keyboardType(UIKeyboardType.decimalPad)
-            .padding(.horizontal)
-            .frame(width: 70, height: 55)
-            .background(Color(UIColor.systemGray5))
-            .cornerRadius(10)
-          Slider(value: Binding(get: {
-            falsePositive
-          }, set: { newObservationFalseLikelihood in
-            falsePositive = newObservationFalseLikelihood
-            listViewModel.updateBelief(belief: belief.updateFalsePositive(falsePositive: newObservationFalseLikelihood))
-          }),
-                 in: 0.0...1.0,
-                 onEditingChanged: { editing in
-                    isEditing = editing
-                 })
-        }
+      }
+      HStack {
+        TextField("", value: binding, formatter: formatter)
+          .keyboardType(UIKeyboardType.decimalPad)
+          .padding(.horizontal)
+          .frame(width: 70, height: 55)
+          .background(Color(UIColor.systemGray5))
+          .cornerRadius(10)
+        Slider(value: binding, in: 0.0...1.0)
       }
     }
-    .padding(14.0)
-    .navigationTitle("Update Belief")
   }
 }
 
